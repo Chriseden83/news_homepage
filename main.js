8e3a9218330e4e5b65baa3e6eb59e69218a56e86
chriseden83 <ceden1983@gmail.com>
2024-09-25 14:58:34 +0100

    Amended Nav overlay, javascript overlay when mobile class open

--- main.js
+++ main.js
@@ -1,23 +1,32 @@
+// Body Element for no scoll when menu open
 const body = document.querySelector('body');
+// Hamburger Icon
 const hamburgerEl = document.querySelector('.nav__open');
+// Nav Menu once burger icon clicked - to show
 const navEl = document.querySelector('.nav__content');
+// Close Icon to remove Nab Menu
 const closeEl = document.querySelector('.nav__content--close');
-const linksEl = document.querySelector('.header__link');
+// Overlay for when Nav Menu is open
+const overlay = document.querySelector('.overlay');
 
+// Burger event listener function to add Nav Menu, Overlay & No scroll
 hamburgerEl.addEventListener('click', () => {
-  navEl.classList.toggle('nav__open');
+  navEl.classList.add('nav__open');
+  overlay.classList.add('visible');
   body.classList.add('no-scroll');
-  body.classList.add('overlay');
 });
 
+// close event listener function to remove Nav Menu, Overlay & No scroll
 closeEl.addEventListener('click', () => {
-  navEl.classList.toggle('nav__open');
+  navEl.classList.remove('nav__open');
+  overlay.classList.remove('visible');
   body.classList.remove('no-scroll');
-  body.classList.remove('overlay');
 });
 
+// nav event listener function to remove Nav Menu, Overlay & No scroll if clicked
 navEl.addEventListener('click', () => {
   navEl.classList.remove('nav__open');
   hamburgerEl.classList.remove('nav__open');
-  body.classList.remove('overlay');
+  overlay.classList.remove('visible');
+  body.classList.remove('no-scroll');
 });
